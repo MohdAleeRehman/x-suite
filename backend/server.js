a4c32d129627daab37d003a7892f1d61dc5259fa
Muhammad Ali <mohdaleerehman@gmail.com>
2026-06-19 03:58:26 +0500

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://x-suite-one.vercel.app/'
+  'https://x-suite-one.vercel.app'
 ];
 
 app.use(cors({
